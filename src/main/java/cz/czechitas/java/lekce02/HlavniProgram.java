50e11e0ad7666e0df3090f706eec8a9813355a87
Zuzana Kaczmarczyková <kaczmarczykova@gmail.com>
2025-04-12 09:16:04 +0200

    domecek i prasatko

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -12,7 +12,52 @@ public class HlavniProgram {
   public void start() {
     zofka = new Turtle();
 
-    //TODO tady bude kód
+    domecek();
+    zofka.turnLeft(45);
+    zofka.move(38);
+    zofka.turnLeft(90);
+    zofka.move(38);
+
+    zofka.penUp();
+    zofka.turnRight(45);
+    zofka.move(100);
+    zofka.penDown();
+    domecek();
+    strecha();
+    zofka.turnRight(15);
+    nohy();
+    zofka.turnRight(70);
+    zofka.penUp();
+    zofka.move(50);
+    zofka.turnRight(50);
+    zofka.penDown();
+    nohy();
   }
 
+  public void domecek() {
+    zofka.penDown();
+    for (int i = 0; i < 4; i++) {
+      zofka.turnLeft(90);
+      zofka.move(50);
+    }
+  }
+
+
+  public void strecha() {
+    zofka.turnLeft(45);
+    zofka.move(38);
+    zofka.turnLeft(90);
+    zofka.move(38);
+  }
+
+
+  public void nohy() {
+    zofka.move(20);
+    zofka.turnRight(120);
+    zofka.penUp();
+    zofka.move(25);
+    zofka.turnRight(120);
+    zofka.penDown();
+    zofka.move(20);
+  }
 }
